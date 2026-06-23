bbba24d9bb424d4a0a2a663cb490475926810f5b
ema.linux <emanueletondi400@gmail.com>
2026-06-23 15:06:09 +0200

    add app icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:explorer_city_app/firebase_options.dart';
 import 'package:explorer_city_app/presentation/screens/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Controlla se siamo su Web, oppure se NON siamo su Linux.
+  // Su Web 'Platform' dà errore, quindi kIsWeb deve essere il primo controllo.
+  if (kIsWeb || !Platform.isLinux) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
   runApp(const MyApp());
 }
 
